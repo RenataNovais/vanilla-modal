cb7ebd186dccb9417b730f9526c531cfac45ebc2
renatanovais <rnovaiscs@gmail.com>
2022-10-29 13:44:02 -0300

    Refactor component to have internal methods and pass info to component after creating it

--- modal.js
+++ modal.js
@@ -1,26 +1,93 @@
+const TEMPLATE = `
+    <style>
+        .modal {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            padding-top: 100px;
+            background-color: rgba(0, 0, 0, 0.7);
+        }
+
+        .modal > div {
+            position: relative;
+            top: 20%;
+            left: 50%;
+            transform: translate(-50%);
+            background-color: #fff;
+            border: 1px solid #888;
+            border-radius: 10px;
+            padding: 25px;
+            width: 50%;
+            min-height: 100px;
+            max-height: 50%;
+            overflow-y: auto;
+        }
+
+        .modal-actions {
+            text-align: right;
+        }
+
+        button {
+            margin: 0 10px;
+        }
+    </style>
+    <div class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+            </div>
+            <div class="modal-actions">
+            </div>
+        </div>
+    </div>
+`;
+
 class CustomModal extends HTMLElement {
-    constructor(content, leftButton, rightButton) {
+    constructor() {
         super();
-        this.content = content;
-        this.leftButton = leftButton;
-        this.rightButton = rightButton;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = TEMPLATE;
     }
 
     connectedCallback() {
-        this.content = this.getAttribute("content");
-        this.leftButton = this.getAttribute("leftButton");
-        this.rightButton = this.getAttribute("rightButton");
-        this.render();
+        this._modal = this.shadowRoot.querySelector('.modal');
+        this._showModal();
+        this._render();
     }
 
-    render() {
-        this.innerHTML = `
-            <div>
-                <p>${this.content}</p>
-                <button>${this.leftButton}</button>
-                <button>${this.rightButton}</button>
-            </div>
-        `;
+    _showModal() {
+        this._modalVisible =true;
+        this._modal.style.display = 'block';
+    }
+
+    _hideModal() {
+        this._modalVisible = false;
+        this._modal.style.display = 'none';
+    }
+
+    _render() {
+        const text = this.shadowRoot.querySelector('.modal-header');
+
+        const content = document.createElement('p');
+        content.textContent = this.content;
+
+        text.appendChild(content);
+
+        const actions = this.shadowRoot.querySelector('.modal-actions');
+
+        this.leftButton = document.createElement('button');
+        this.leftButton.textContent = this.leftText;
+        
+        actions.appendChild(this.leftButton);
+
+        this.rightButton = document.createElement('button');
+        this.rightButton.textContent = this.rightText;
+        
+        actions.appendChild(this.rightButton);
     }
 }
 
